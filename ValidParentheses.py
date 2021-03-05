686dee6e46eaa50833014607343d5a3d9e1c4a13
vincehientran <35750052+vincehientran@users.noreply.github.com>
2021-03-05 00:22:06 -0600

    Update ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -9,12 +9,14 @@ class Solution(object):
 
         for paren in s:
 
+            # put all open parens in the stack
             if paren == '(' or paren == '[' or paren == '{':
                 stack.append(paren)
             else:
                 if len(stack) == 0:
                     return False
 
+                # pop the stack and check if it is the open pair for the closing parens
                 prevOpen = stack.pop(len(stack)-1)
 
                 if paren == ')' and not prevOpen == '(':
@@ -23,7 +25,6 @@ class Solution(object):
                     return False
                 elif paren == '}' and not prevOpen == '{':
                     return False
-
 
         if len(stack) > 0:
             return False
