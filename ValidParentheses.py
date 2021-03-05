072569df9c3f9977480514bba43213b2dba37f17
vincehientran <>
2021-03-04 21:24:37 -0600

    Create ValidParentheses.py

---
+++ ValidParentheses.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+
+        for paren in s:
+
+            if paren == '(' or paren == '[' or paren == '{':
+                stack.append(paren)
+            else:
+                if len(stack) == 0:
+                    return False
+
+                prevOpen = stack.pop(len(stack)-1)
+
+                if paren == ')' and not prevOpen == '(':
+                    return False
+                elif paren == ']' and not prevOpen == '[':
+                    return False
+                elif paren == '}' and not prevOpen == '{':
+                    return False
+
+
+        if len(stack) > 0:
+            return False
+
+        return True
