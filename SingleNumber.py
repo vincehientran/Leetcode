e6cf30f00f14d8d501c1441a795ac8c6c4fc5381
vincehientran <vincehientran@gmail.com>
2021-04-19 00:35:24 -0500

    learning something new with reduce() and XOR

---
+++ SingleNumber.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        return reduce(operator.xor, nums)
